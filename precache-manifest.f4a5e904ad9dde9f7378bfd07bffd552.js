be0fe83b28ce5f7d2613d6a106e05efb3de8784c
cc1254 <cc1254@dupont.com>
2019-01-30 15:46:29 +0530

    Updates

---
+++ precache-manifest.f4a5e904ad9dde9f7378bfd07bffd552.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "672e285b653fa998f0f9",
+    "url": "/react-tutorial/static/js/main.672e285b.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "672e285b653fa998f0f9",
+    "url": "/react-tutorial/static/css/main.42f2bd9d.chunk.css"
+  },
+  {
+    "revision": "7ba3a8de293cc709eb6dc51c435efe1b",
+    "url": "/react-tutorial/index.html"
+  }
+];
